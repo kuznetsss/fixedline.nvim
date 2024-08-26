1b95c349ae7b4b3ed14535c2916d099e6d5159cb
Sergey <kuzzz99@gmail.com>
2024-08-26 20:00:52 +0100

    Apply fotmatting

--- lua/fixedline/line.lua
+++ lua/fixedline/line.lua
@@ -5,27 +5,27 @@ local make_line = function()
     local context = {
         win_id = win_id,
         buf_id = vim.api.nvim_win_get_buf(win_id),
-        is_current = win_id == vim.api.nvim_get_current_win()
+        is_current = win_id == vim.api.nvim_get_current_win(),
     }
-  return table.concat {
-    components.mode.str(context),
-    components.ro.str(context),
-    '%#FixedLine_Filename# %t ',
-    components.modified.str(context),
-    components.git.str(context),
-    '%#StatusLine#%=',
-    components.recording_macro.str(),
-    components.copilot.str(context),
-    '  ',
-    components.diagnostic.str(context),
-    '  ',
-    components.filetype.str(context),
-    ' ',
-    components.mode.get_highlight(context),
-    ' %l:%c | %p%% ',
-  }
+    return table.concat {
+        components.mode.str(context),
+        components.ro.str(context),
+        '%#FixedLine_Filename# %t ',
+        components.modified.str(context),
+        components.git.str(context),
+        '%#StatusLine#%=',
+        components.recording_macro.str(),
+        components.copilot.str(context),
+        '  ',
+        components.diagnostic.str(context),
+        '  ',
+        components.filetype.str(context),
+        ' ',
+        components.mode.get_highlight(context),
+        ' %l:%c | %p%% ',
+    }
 end
 
 return {
-  make_line = make_line
+    make_line = make_line,
 }
